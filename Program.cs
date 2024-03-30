cb2ed3be6bf4cee465610285e827f3d5acc78d0e
Bharath <122342020+Bharath-hck@users.noreply.github.com>
2024-03-30 12:14:02 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Added a new line");
-        Console.WriteLine("Added another line");
-        Console.WriteLine("Added new change from develop branch");
-        Console.WriteLine("Added new user login feature from feature branch");
-        Console.WriteLine("Added this change in remote repo");
+        Console.WriteLine("Hello, World!");       
     }
 }
